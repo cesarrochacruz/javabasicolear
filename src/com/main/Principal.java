20dc1191064ad789213996c9aa8188fddd24393c
cesarrochacruz <39930687+cesarrochacruz@users.noreply.github.com>
2018-07-18 14:42:12 -0300

    primer clon

---
+++ src/com/main/Principal.java
@@ -0,0 +1,21 @@
+package com.main;
+
+import com.calculadora.Suma;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+		int valorA = 5;
+		int valorB= 10;
+		int resultado = 0;
+		
+		
+		Suma suma = new Suma();
+		resultado = suma.operacion(valorA, valorB);
+
+		System.out.println("Resultado de suma: " + resultado);
+	}
+
+}

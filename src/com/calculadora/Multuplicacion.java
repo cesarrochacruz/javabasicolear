545ea94b1115d0aae58d37bcc22bb100ab73b9f7
kennyscsk <34349861+kennyscsk@users.noreply.github.com>
2018-07-20 16:19:16 -0300

    clase de multiplicacion
    creacrion de clase de multiplicacion

---
+++ src/com/calculadora/Multuplicacion.java
@@ -0,0 +1,15 @@
+package com.calculadora;
+
+public class Multuplicacion {
+	
+		public Multuplicacion() {
+					
+		}
+		
+		public int operacion(int a,int b) {
+				int resultado = 0;
+				resultado = a * b;
+				return resultado;
+			
+		}
+	}
--- src/com/main/Principal.java
+++ src/com/main/Principal.java
@@ -12,16 +12,23 @@ public class Principal {
 		int valorB= 10;
 		int resultado = 0;
 		
+//		double resultado = 0;
 		
 		Suma suma = new Suma();
-		resultado = suma.operacion(valorA, valorB);
+		resultado= suma.operacion(valorA, valorB);
+		
+		System.out.println("Resultado de suma: " + resultado );
+	
+		
+		Multuplicacion multiplicar = new Multuplicacion ();
+		resultado= multiplicar.operacion(5, 2);
+				
+		System.out.println("Resultado de multiplicacion: " + resultado );
 		
 		Resta resta = new Resta();
 		resultado = resta.operacion(16, 18);
-
-		System.out.println("Resultado de suma: " + resultado);
-		System.out.println("hola mundo");
-		System.out.println("Resultado de resta: " + resultado);
+	
+		System.out.println("Resultado de resta: " + resultado );
 	}
 
 }

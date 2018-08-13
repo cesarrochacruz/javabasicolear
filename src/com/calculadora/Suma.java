726daecca451408e8db8d3cb71d332dd9e5d1c37
Magdy1201 <magdaly1201@gmail.com>
2018-08-13 13:38:25 -0300

    Add files via upload

--- src/com/calculadora/Suma.java
+++ src/com/calculadora/Suma.java
@@ -1,5 +1,7 @@
 package com.calculadora;
 
+import java.util.List;
+
 public class Suma {
 	public Suma() {
 		
@@ -10,5 +12,26 @@ public class Suma {
 			resultado = a + b;
 			return resultado;
 		
+	}
+	public double operacion(double a,double b) {
+		double resultado = 0;
+		resultado = a + b;
+		return resultado;
+	}
+	
+	public int sumarArreglo(int sumar[]) {
+		int resultado = 0;
+		
+		for(int i=0; i<sumar.length; i++){
+			resultado=resultado + sumar[i];
+		} 
+		return resultado;
+	}
+	public int sumaColeccion(List<Integer> lista) {
+		int suma = 0;
+		for(int i=0; i<lista.size();i++) {
+			suma= suma + lista.get(i);
+		}
+		return suma;
 	}
 }
--- src/com/main/Principal.java
+++ src/com/main/Principal.java
@@ -1,21 +1,89 @@
 package com.main;
 
 import com.calculadora.Suma;
+import com.calculadora.Resta;
+import com.calculadora.Multiplicacion;
+import com.calculadora.Division;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
+	
+		int valorAint = 15;
+		int valorBint= 10;
+		int resultadoInt = 0;
 		
-		int valorA = 5;
-		int valorB= 10;
-		int resultado = 0;
+		System.out.println("valor A : "+valorAint);
+		System.out.println("valor B : "+valorBint);
 		
-		
+		//SUMA DE VALORES INT A Y B 
 		Suma suma = new Suma();
-		resultado = suma.operacion(valorA, valorB);
+		resultadoInt = suma.operacion(valorAint, valorBint);
+		System.out.println("Resultado de suma int: " + resultadoInt);
+		
+		//RESTA DE VALORES INT A Y B 
+		Resta resta = new Resta();
+		resultadoInt = resta.operacion(valorAint, valorBint);
+		System.out.println("Resultado de Resta int: " + resultadoInt);
+		
+		//MULTIPLICACION DE VALORES INT A Y B 
+		Multiplicacion multiplicacion = new Multiplicacion();
+		resultadoInt = multiplicacion.operacion(valorAint,valorBint);
+		System.out.println("Resultado de multiplicacion int: " + resultadoInt);
 
-		System.out.println("Resultado de suma: " + resultado);
+		//DIVISION DE VALORES INT A Y B 
+		Division division = new Division();
+		resultadoInt = division.operacion(valorAint, valorAint);
+		System.out.println("Resultado de division int: "+ resultadoInt);
+		
+		
+
+		double valorAdouble = 1.4;
+		double valorBdouble= 10.1;
+		double resultadoDouble = 0;
+		
+		System.out.println("valor A : "+valorAdouble);
+		System.out.println("valor B : "+valorBdouble);
+		
+		//SUMA DE VALORES DOUBLE A Y B 
+		resultadoDouble = suma.operacion(valorAdouble, valorBdouble);
+		System.out.println("Resultado de suma Double: " + resultadoDouble);
+		
+		//RESTA DE VALORES INT A Y B 
+		resultadoDouble = resta.operacion(valorAdouble, valorBdouble);
+		System.out.println("Resultado de Resta Double : " + resultadoDouble);
+		
+		//MULTIPLICACION DE VALORES INT A Y B 
+		resultadoDouble = multiplicacion.operacion(valorAdouble,valorBdouble);
+		System.out.println("Resultado de multiplicacion Double: " + resultadoDouble);
+
+		//DIVISION DE VALORES INT A Y B 
+		resultadoDouble = division.operacion(valorAdouble, valorBdouble);
+		System.out.println("Resultado de division Double "+ resultadoDouble);
+		
+		// SUMAR VALORES DE UN ARREGLO 
+		int resultado;
+		int sumar[] = {4,3,5,7,10,1};
+		
+		resultado =  suma.sumarArreglo(sumar);
+		System.out.println("Resultado de suma de arreglos : "+resultado);
+		
+		
+		// COLECCIONES ARRAYLIST
+		List<Integer> lista = new ArrayList<Integer>();
+		
+		lista.add(4);
+		lista.add(4);
+		lista.add(4);
+	
+		int resultadoListaColeccion = suma.sumaColeccion(lista);
+		
+		System.out.println("valores"+resultadoListaColeccion);
+		
 	}
 
 }
